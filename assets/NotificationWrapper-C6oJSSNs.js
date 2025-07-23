7c32d456ae6d09cee1fc2c1642dd0b13f4c622a7
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-07-23 14:05:30 +0800

    update

---
+++ assets/NotificationWrapper-C6oJSSNs.js
@@ -0,0 +1,1 @@
+import{o as r,E as i}from"./index-Br1_NATY.js";const c={__name:"NotificationWrapper",setup(n){const t=i();function e(){return!!(window.matchMedia("(orientation: portrait)").matches||window.innerWidth<=768)}return r(()=>{e()?t.replace({name:"notification-mobile"}):t.replace({name:"notification-desktop"})}),(o,a)=>null}};export{c as default};
