affa905d278a89d014fd12155240bf8f185dbcc7
Mukarabin Ali Amu <129189189+mukarabin@users.noreply.github.com>
2025-08-01 09:45:50 +0800

    update

--- assets/NotificationWrapper-DIOlTq0h.js
+++
@@ -1,1 +0,0 @@
-import{o as r,H as i}from"./index-BZPVUeNE.js";const c={__name:"NotificationWrapper",setup(n){const t=i();function e(){return!!(window.matchMedia("(orientation: portrait)").matches||window.innerWidth<=768)}return r(()=>{e()?t.replace({name:"notification-mobile"}):t.replace({name:"notification-desktop"})}),(o,a)=>null}};export{c as default};
